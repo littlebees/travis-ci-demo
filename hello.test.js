9088f6eaaeb2a225a518c4b2f22b243c9b0e2576
bee314 <bee314@DESKTOP-UKSNVOF.localdomain>
2020-03-16 22:20:21 +0800

    test ci3

--- hello.test.js
+++ hello.test.js
@@ -1,13 +1,13 @@
 'use strict';
 
 var expect = require('chai').expect;
-var {hello, node} = require('./hello');
+var hi = require('./hello');
 
 describe('run hello.js', function() {
   it('should return "Hello World"', function() {
-    expect(hello()).to.equal('Hello World');
+    expect(hi.hello()).to.equal('Hello World');
   });
   it('should return "Hello Node"', function() {
-    expect(node()).to.equal('Hello Node');
+    expect(hi.node()).to.equal('Hello Node');
   });
 });
